345cc020aef81b4b111894bef28e11f6b80de52a
mazdak <nazemi.works@gmail.com>
2019-05-16 03:37:11 -0700

    react spliting routs/code optimization reactrouter systemimport

--- src/router.js
+++ src/router.js
@@ -3,21 +3,60 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import Home from './components/Home';
 import ArtistMain from './components/artists/ArtistMain';
-import ArtistDetail from './components/artists/ArtistDetail';
-import ArtistCreate from './components/artists/ArtistCreate';
-import ArtistEdit from './components/artists/ArtistEdit';
+// import ArtistDetail from './components/artists/ArtistDetail';
+// import ArtistCreate from './components/artists/ArtistCreate';
+// import ArtistEdit from './components/artists/ArtistEdit';
+//code splitting for optimization for load time
+//change jsx to plain routs by obj
+const componentRoutes = {
+  component : Home,
+  path : '/',
+  indexRoute : { component : ArtistMain },
+  childRoutes : [
+    {
+      path : 'artists/new',
+      //from react router
+      getComponent(location ,cb){
+        // its for load up components async.
+        //use system import then after call comp with cb func
+        System.import('./components/artists/ArtistCreate') // when react see it auto split code 
+          .then(module => cb(null, module.default)); //when we run this code all code is there only
+      }
+    },
+    {
+      path : 'artists/:id',
+      getComponent(location ,cb){
+        System.import('./components/artists/ArtistDetail') // when react see it auto split code 
+          .then(module => cb(null, module.default)); //when we run this code all code is there only
+      }
+    },
+    {
+      path : 'artists/:id/edit',
+      getComponent(location ,cb){
+        System.import('./components/artists/ArtistEdit') // when react see it auto split code 
+          .then(module => cb(null, module.default)); //when we run this code all code is there only
+      }
+    }
+  ]
+};
+
 
 const Routes = () => {
   return (
-    <Router history={hashHistory}>
-      <Route path="/" component={Home}>
-        <IndexRoute component={ArtistMain} />
-        <Route path="artists/new" component={ArtistCreate} />
-        <Route path="artists/:id" component={ArtistDetail} />
-        <Route path="artists/:id/edit" component={ArtistEdit} />
-      </Route>
-    </Router>
+    <Router history={hashHistory} routes={componentRoutes} />
   );
 };
+// const Routes = () => {
+//   return (
+//     <Router history={hashHistory}>
+//       <Route path="/" component={Home}>
+//         <IndexRoute component={ArtistMain} />
+//         <Route path="artists/new" component={ArtistCreate} />
+//         <Route path="artists/:id" component={ArtistDetail} />
+//         <Route path="artists/:id/edit" component={ArtistEdit} />
+//       </Route>
+//     </Router>
+//   );
+// };
 
 export default Routes;
